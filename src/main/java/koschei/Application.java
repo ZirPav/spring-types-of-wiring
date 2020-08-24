56e2c6c0322ad5e33cbae4158b35fe727c387d72
zirpav <zir-pav@mail.ru>
2020-08-24 17:29:08 +0300

    v.1.0

--- src/main/java/koschei/Application.java
+++ src/main/java/koschei/Application.java
@@ -5,9 +5,12 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Application {
+
     public static void main(String[] args) {
+//"На свете есть океан , на океане остров , на острове дерево , на дереве заяц , в зайце утка , в утке яйцо , в яйце иголка , смерть Кощея на игле :("
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         KoscheiTheDeathless koscheiTheDeathless =
                 applicationContext.getBean(KoscheiTheDeathless.class);
         System.out.println(koscheiTheDeathless.getRulesByDeth());
